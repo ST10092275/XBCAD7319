2b963e6abf1509a54dac3a446284268e3355bdd9
ST10092275 <104361577+ST10092275@users.noreply.github.com>
2024-11-12 22:37:01 +0200

    Revert "Merge branch 'main' of https://github.com/ST10092275/XBCAD7319"
    This reverts commit c971d92f847b2810c72cc0423c4cf832a905f67e, reversing
    changes made to 5baec3fe39b4495d2fb3b6ace4a9fd5e81336098.

--- app/src/main/java/com/example/xbcad7319/data/model/Message.kt
+++
@@ -1,9 +0,0 @@
-package com.example.xbcad7319.data.model
-
-// Message.kt
-data class Message(
-    val id: String, // Unique identifier for the message
-    val senderId: String, // ID of the user who sent the message
-    val content: String, // The actual message content
-    val timestamp: Long // Time the message was sent
-)
